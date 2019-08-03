6e67e5a21dcaadbb7351ec05e1c7af2565da6a7b
Ali1 <ali.m.bakir@gmail.com>
2019-08-03 17:55:49 +0000

    logging

--- plexlistenandwol.py
+++ plexlistenandwol.py
@@ -18,7 +18,19 @@ class Object(object):
 conf=Object()
 conf.mac = '60:45:cb:82:06:d0' # ali
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ])
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(nprintame)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
 def main():
+    logging.info('Starting app');
     # Check for root
     if os.geteuid():
         exit('[-] Please run as root')
@@ -30,59 +42,58 @@ def main():
     host = ''
     port = 32400
     
-    print("Attempting to bind port");
+    logging.info("Attempting to bind port");
     try:
         s.bind((host, port))
     except socket.error as e:
         if e.errno == 98:
-            print("Port is already in use. Shutting Plex and trying in 45 secs")
+            logging.warning("Port is already in use. Shutting Plex and trying in 45 secs")
             os.system("sudo service plexmediaserver stop")
             s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
             time.sleep(45)
-            print("Now try again")
+            logging.info("Now try again")
             try:
                 s.bind((host, port))
             except:
-                print("Did not work, trying to kill the process")
+                logging.error("Did not work, trying to kill the process")
                 os.system("pid=$(lsof -i:32400 -t); kill -TERM $pid || kill -KILL $pid")
                 time.sleep(45)
-                print("Ok, last try")
+                logging.info("Ok, last try")
                 s.bind((host, port))
         else:
             raise
 
     s.settimeout(45);
     s.listen(1) # up to 2 connections
-    print("Binded successfully");
+    logging.info("Binded successfully");
     first_run=1 # to prevent the Plex shutdown process from occurring right away
     while True:
         now = datetime.datetime.now()
-        print (now.hour , now.minute , now.second)
         if now.hour==9 and now.minute==49 and first_run==0:
-            print("shutting sockets down to prepare for plex");
+            logger.info("shutting sockets down to prepare for plex");
             try:
                 s.shutdown(socket.SHUT_RDWR)
             except:
                 pass
             s.close()
             time.sleep(60)
-            print("starting plex for 60 seconds");
+            logger.info("starting plex for 60 seconds");
             os.system("sudo service plexmediaserver start")
             time.sleep(60)
-            print("stopping plex");
+            logger.info("stopping plex");
             os.system("sudo service plexmediaserver stop")
             time.sleep(60)
-            print("re-opening sockets")
+            logger.info("re-opening sockets")
             s = socket.socket(socket.AF_INET,
                               socket.SOCK_STREAM)
             s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
             try:
                 s.bind((host, port))
             except:
-                print("Did not work, trying to kill processes hogging port 32400")
+                logger.error("Did not work, trying to kill processes hogging port 32400")
                 os.system("pid=$(lsof -i:32400 -t); kill -TERM $pid || kill -KILL $pid")
                 time.sleep(45)
-                print("Ok, last try")
+                logger.info("Ok, last try")
                 s.bind((host, port))
             s.settimeout(45);
             s.listen(1) # up to 2 connections
@@ -94,7 +105,7 @@ def main():
         except:
             raise
         else:
-            print("got a connection from %s" % str(addr))
+            logger.info("got a connection from %s" % str(addr))
             clientSocket.close()
             wake_on_lan(conf.mac)
             time.sleep(1)
@@ -108,10 +119,11 @@ def main():
             time.sleep(1)
             wake_on_lan(conf.mac)
             time.sleep(1)
-            print 'sent 6 WOLs to ' + conf.mac + '. Now sleeping'
+            logging.info('sent 6 WOLs to ' + conf.mac + '. Now sleeping');
             time.sleep(10)
-    print("dying")
+    logger.info("dying")
 
 
 if __name__ == "__main__":
    main()
+
